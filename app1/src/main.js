560760c657b84f7d7577f2757096efb0fba50f4f
DESKTOP-P6QN5A2\admin <849609430@qq.com>
2020-11-20 09:21:38 +0800

    修改mian

--- app1/src/main.js
+++ app1/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
 
+
+Vue.config.productionTip = false
+console.log("文件被修改")
 new Vue({
   router,
   store,
