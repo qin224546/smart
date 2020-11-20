01b338aaa7f050b54ec71a7b73de1a6fe082edc0
DESKTOP-P6QN5A2\admin <849609430@qq.com>
2020-11-20 15:37:37 +0800

    Merge branch 'qin' into json
    # Conflicts:
    #	app1/src/views/Asds.vue

--- app1/src/views/Asds.vue
+++ app1/src/views/Asds.vue
@@ -1,7 +1,4 @@
-console.log("666666")
-<<<<<<< HEAD
-console.log("徐洋留下的足迹")
-=======
+
 console.log("666666")
 console.log("666666")
 console.log("666666")
@@ -10,4 +7,3 @@ console.log("666666")
 console.log("666666")
 console.log("666666")
 console.log("徐洋留下的足迹")
->>>>>>> qin
